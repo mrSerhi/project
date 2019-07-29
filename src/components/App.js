ce801a3824dd2b945e3d444d0952f406ae1ca098
Serhii <serhii.mar90@gmail.com>
2019-07-29 14:26:21 +0300

    replace navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-
 import Navigation from "./Navigation";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./UI/Navbar";
 
 class App extends Component {
   render() {
-    return <Navigation />;
+    return (
+      <Router>
+        <Navbar />
+        <Navigation />
+      </Router>
+    );
   }
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 // components
-import TodoNavbar from "./todos/TodoNavbar";
 import Todo from "./pages/TodoPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 
 const Navigation = () => {
   return (
-    <Router>
-      <TodoNavbar />
-      <Switch>
-        <Route path="/" exact component={Todo} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/sign-up" component={SignUpPage} />
-        <Route render={() => <h1>Page is not found...</h1>} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={Todo} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/sign-up" component={SignUpPage} />
+      <Route render={() => <h1>Page is not found...</h1>} />
+    </Switch>
   );
 };
 
