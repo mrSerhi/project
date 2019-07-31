e0c25e5e01f4881383ab281d8d428fd24f1aef2a
Serhii <serhii.mar90@gmail.com>
2019-07-31 11:03:12 +0300

    logging on email and username. fix done

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -17,25 +17,36 @@ const signInSchema = Yup.object().shape({
   email: Yup.string()
     .email("Type a valid email address")
     .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password has to be longer than 6 characters!")
-    .required("Password is required")
+  // password: Yup.string()
+  //   .min(6, "Password has to be longer than 6 characters!")
+  //   .required("Password is required"),
+  username: Yup.string()
+    .min(3, "Name should be longest than 3 characters")
+    .max(20, "Name should be shorter than 20 characters")
+    .required("Name is required")
 });
 
 class LoginForm extends Component {
-  state = { password: "", email: "", isLogged: false };
+  // state = { password: "", email: "", username: "", isLogged: false };
+  state = { email: "", username: "", isLogged: false };
 
   handleSubmitForm = (values, actions) => {
-    const { email, password } = values;
+    const { email, password, username } = values;
     const users = JSON.parse(localStorage.getItem("users"));
     const errors = {};
-    let currentUser = users.find(
-      (user) => user.email === email && user.password === password
-    );
+    let currentUser = users.find((user) => user.email === email);
 
     if (currentUser === undefined) {
-      errors.email = "User is not found...Try to change email and password";
+      errors.email = "User is not found...Try to change email";
     }
+
+    if (currentUser !== undefined && currentUser.username !== username) {
+      errors.username = "Username is wrong. Type yours account username";
+    }
+
+    // if (currentUser !== undefined && currentUser.password !== password) {
+    //   errors.password = "Password is not valid. Type yours account password";
+    // }
 
     if (!Object.keys(errors).length) {
       // set to localStorage currently logged user
@@ -73,79 +84,83 @@ class LoginForm extends Component {
     }
 
     return (
-      <Container className="mb-5">
-        <Row>
-          <Col sm={{ span: 8, offset: 2 }} className="mt-5">
-            <Alert variant="warning" className="mb-5 text-center">
-              <p className="lead">To use the Todos you must be authorized</p>
-              <p className="lead">
-                If you not registered yet, you can follow{" "}
-                <Link to="/sign-up">Sign Up</Link> and create accound
-              </p>
-            </Alert>
-          </Col>
-        </Row>
+      <>
+        <Container className="mb-5">
+          <Row>
+            <Col sm={{ span: 8, offset: 2 }} className="mt-5">
+              <Alert variant="warning" className="mb-5 text-center">
+                <p className="lead">To use the Todos you must be authorized</p>
+                <p className="lead">
+                  If you not registered yet, you can follow{" "}
+                  <Link to="/sign-up">Sign Up</Link> and create accound
+                </p>
+              </Alert>
+            </Col>
+          </Row>
 
-        <Row>
-          <Col sm={{ span: 6, offset: 3 }} className="mt-2">
-            <Card>
-              <Card.Body>
-                <h3 className="display-4 text-center">Log In</h3>
+          <Row>
+            <Col sm={{ span: 6, offset: 3 }} className="mt-2">
+              <Card>
+                <Card.Body>
+                  <h3 className="display-4 text-center">Log In</h3>
 
-                <Formik
-                  initialValues={this.state}
-                  onSubmit={this.handleSubmitForm}
-                  validationSchema={signInSchema}
-                >
-                  {({
-                    handleSubmit,
-                    handleChange,
-                    values,
-                    errors,
-                    touched
-                  }) => (
-                    <Form noValidate onSubmit={handleSubmit}>
-                      <Form.Group controlId="validationUserEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="email"
-                          value={values.email}
-                          onChange={handleChange}
-                          isInvalid={!!errors.email && touched.email}
-                          placeholder="your.awesome@gmail.com"
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          {errors.email}
-                        </Form.Control.Feedback>
-                      </Form.Group>
+                  <Formik
+                    initialValues={this.state}
+                    onSubmit={this.handleSubmitForm}
+                    validationSchema={signInSchema}
+                  >
+                    {({
+                      handleSubmit,
+                      handleChange,
+                      values,
+                      errors,
+                      touched
+                    }) => (
+                      <Form noValidate onSubmit={handleSubmit}>
+                        <Form.Group controlId="validationUserEmail">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                            isInvalid={!!errors.email && touched.email}
+                            placeholder="your.awesome@gmail.com"
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.email}
+                          </Form.Control.Feedback>
+                        </Form.Group>
 
-                      <Form.Group controlId="validationPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          name="password"
-                          value={values.password}
-                          onChange={handleChange}
-                          isInvalid={!!errors.password && touched.password}
-                          placeholder="Password"
-                        />
-                        <Form.Control.Feedback type="invalid">
-                          {errors.password}
-                        </Form.Control.Feedback>
-                      </Form.Group>
+                        <Form.Group controlId="validationUsername">
+                          <Form.Label>Username</Form.Label>
+                          <Form.Control
+                            type="text"
+                            name="username"
+                            value={values.username}
+                            onChange={handleChange}
+                            isInvalid={!!errors.username && touched.username}
+                            placeholder="Username"
+                          />
+                          <Form.Control.Feedback type="invalid">
+                            {errors.username}
+                          </Form.Control.Feedback>
+                        </Form.Group>
 
-                      <Button type="submit" variant="info">
-                        Sign In
-                      </Button>
-                    </Form>
-                  )}
-                </Formik>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+                        <Button type="submit" variant="info">
+                          Log In
+                        </Button>
+
+                        <Button variant="link">Forgot email?</Button>
+                      </Form>
+                    )}
+                  </Formik>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
