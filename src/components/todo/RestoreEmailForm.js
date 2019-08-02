6c4969ff7af8dded9b4b655edeb359b9d0868c85
Serhii <serhii.mar90@gmail.com>
2019-08-02 18:32:49 +0300

    small changes for restore email. next -> correct get out feedback in this component when email will be restored

--- src/components/todo/RestoreEmailForm.js
+++ src/components/todo/RestoreEmailForm.js
@@ -2,47 +2,33 @@ import React, { Component } from "react";
 import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { Link } from "react-router-dom";
+import FormInput from "../ui/FormInput";
 
 const restoreEmailSchema = Yup.object().shape({
   username: Yup.string()
     .min(3, "Name should be longest than 3 characters")
     .max(20, "Name should be shorter than 20 characters")
-    .required("Name is required"),
-  password: Yup.string()
-    .min(6, "Password has to be longer than 6 characters!")
-    .required("Password is required!")
+    .required("Name is required")
 });
 
 class RestoreEmailForm extends Component {
   state = {
     username: "",
     password: "",
-    restored: false,
-    email: "",
-    notFondedUser: false
+    sendVerification: false
   };
 
   handleSubmitForm = (values, actions) => {
     const users = JSON.parse(localStorage.getItem("users"));
-    const { username, password } = values;
-    const foundUser = users.find((user) => user.username === username);
+    const user = users.find((user) => user.username === values.username);
     const errors = {};
 
-    if (foundUser === undefined) {
-      this.setState({ notFondedUser: true, restored: false });
-      return;
-    } else if (foundUser !== undefined && foundUser.password !== password) {
-      errors.password = "Wrong password. Type yours account password";
+    if (user === undefined) {
+      errors.username = "No user with this name found";
     }
 
     if (!Object.keys(errors).length) {
-      // set restored email address to state
-      this.setState({
-        email: foundUser.email,
-        restored: true,
-        notFondedUser: false
-      });
+      this.setState({ sendVerification: true });
       actions.resetForm(this.state);
     } else {
       return actions.setErrors(errors);
@@ -58,7 +44,6 @@ class RestoreEmailForm extends Component {
               <p className="lead text-center">
                 For restoring email you should enter yours currently{" "}
                 <span className="text-danger">Username</span> and{" "}
-                <span className="text-danger">Password</span>
               </p>
             </Alert>
 
@@ -69,49 +54,17 @@ class RestoreEmailForm extends Component {
             >
               {({ handleSubmit, handleChange, values, errors, touched }) => (
                 <Form noValidate onSubmit={handleSubmit}>
-                  <Form.Group controlId="validationUsernameRestoreForm">
-                    <Form.Label>
-                      Username<span className="text-danger">*</span>
-                    </Form.Label>
-                    <Form.Control
-                      type="text"
-                      name="username"
-                      value={values.username}
-                      onChange={handleChange}
-                      isInvalid={!!errors.username && touched.username}
-                      isValid={touched.username && !errors.username}
-                      placeholder="Type your username"
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {errors.username}
-                    </Form.Control.Feedback>
-
-                    <Form.Control.Feedback>
-                      Username accepted
-                    </Form.Control.Feedback>
-                  </Form.Group>
-
-                  <Form.Group controlId="validationPasswordRestoreForm">
-                    <Form.Label>
-                      Password<span className="text-danger">*</span>
-                    </Form.Label>
-                    <Form.Control
-                      type="password"
-                      name="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      isInvalid={!!errors.password && touched.password}
-                      isValid={touched.password && !errors.password}
-                      placeholder="Type your password"
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {errors.password}
-                    </Form.Control.Feedback>
-
-                    <Form.Control.Feedback>
-                      Password accepted
-                    </Form.Control.Feedback>
-                  </Form.Group>
+                  <FormInput
+                    id="validation-username-restore-form"
+                    type="text"
+                    name="username"
+                    value={values.username}
+                    onChange={handleChange}
+                    errors={errors}
+                    touched={touched}
+                    label="Username"
+                    placeholder="Type your username"
+                  />
 
                   <Button type="submit" variant="info">
                     Restore
@@ -119,20 +72,6 @@ class RestoreEmailForm extends Component {
                 </Form>
               )}
             </Formik>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col className="mt-5">
-            <Alert variant="success">
-              Your email: {this.state.restored && <b>{this.state.email}</b>}
-            </Alert>
-
-            {!!this.state.notFondedUser && (
-              <Alert variant="danger">
-                User is not found... Try <Link to="/sign-up">Sign Up</Link>
-              </Alert>
-            )}
           </Col>
         </Row>
       </Container>
